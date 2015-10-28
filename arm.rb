06ffd853162339c0dbc2c94cd77e2cde8e394681
Manuel Franco <mfranco@flexiant.com>
2015-10-28 14:49:19 +0100

    Example of creating a VM, including security rules

--- arm.rb
+++ arm.rb
@@ -26,8 +26,53 @@ def authenticate(tenant_id, client_id, secret)
   credentials = MsRest::TokenCredentials.new(token_provider)
 end
 
-def create_storage_profile
-  raise NotImplementedError
+def create_storage_profile(storage_client, resource_group)
+  storage_profile = StorageProfile.new
+  storage_profile.image_reference = get_image_reference
+  storage = create_storage_account(storage_client, resource_group)
+  os_disk = OSDisk.new
+  os_disk.caching = 'None'
+  os_disk.create_option = 'fromImage'
+  os_disk.name = 'Test'
+  virtual_hard_disk = VirtualHardDisk.new
+  virtual_hard_disk.uri = generate_os_vhd_uri storage.name
+  os_disk.vhd = virtual_hard_disk
+  storage_profile.os_disk = os_disk
+  storage_profile
+end
+
+def generate_os_vhd_uri(storage_name)
+  container_name = 'cont-' + TEST_NAME
+  vhd_container = "https://#{storage_name}.blob.core.windows.net/#{container_name}"
+  os_vhduri = "#{vhd_container}/#{TEST_NAME}.vhd"
+  os_vhduri
+end
+
+def get_image_reference
+  ref = ImageReference.new
+  ref.publisher = 'Canonical'
+  ref.offer = 'UbuntuServer'
+  ref.sku = '14.04.3-LTS'
+  ref.version = 'latest'
+  ref
+end
+
+def create_storage_account(storage_client, resource_group)
+  storage_name = 'storage' + TEST_NAME
+  params = build_storage_account_create_parameters(storage_name, resource_group.location)
+  result = storage_client.storage_accounts.create(resource_group.name, storage_name, params).value!.body
+  result.name = storage_name #similar problem in dot net tests
+  result
+end
+
+def build_storage_account_create_parameters(name, location)
+  params = Azure::ARM::Storage::Models::StorageAccountCreateParameters.new
+  params.location = location
+  params.name = name
+  props = Azure::ARM::Storage::Models::StorageAccountPropertiesCreateParameters.new
+  params.properties = props
+  props.account_type = 'Standard_GRS'
+  params
 end
 
 def create_network_profile(network_client, resource_group)
@@ -65,36 +110,66 @@ end
 
 def build_network_interface_param(network_client, resource_group, subnet)
   puts "     - building params"
+  network_interface_name = 'nic-' + TEST_NAME
+  ip_configuration = create_ip_configuration(network_client, resource_group, subnet)
+  nsg = create_network_security_group(network_client, resource_group)
+  props = NetworkInterfacePropertiesFormat.new
+  props.ip_configurations = [ip_configuration]
+  props.network_security_group = nsg
   params = NetworkInterface.new
   params.location = resource_group.location
-  network_interface_name = 'nic-' + TEST_NAME
-  ip_config_name = 'ip_name-' + TEST_NAME
   params.name = network_interface_name
-  props = NetworkInterfacePropertiesFormat.new
-  ip_configuration = NetworkInterfaceIpConfiguration.new
   params.properties = props
-  props.ip_configurations = [ip_configuration]
-  ip_configuration_properties = NetworkInterfaceIpConfigurationPropertiesFormat.new
-  ip_configuration.properties = ip_configuration_properties
-  ip_configuration.name = ip_config_name
-  ip_configuration_properties.private_ipallocation_method = 'Dynamic'
-  puts "     - creating public ip"
-  ip_configuration_properties.public_ipaddress = create_public_ip_address(network_client, resource_group)
-  puts "     - done"
-  ip_configuration_properties.subnet = subnet
   params
 end
 
+def create_network_security_group(network_client, resource_group)
+  props = NetworkSecurityGroupPropertiesFormat.new
+  nsg = NetworkSecurityGroup.new
+  nsg.properties = props
+  nsg.location = resource_group.location
+  nsg = network_client.network_security_groups.create_or_update(resource_group.name, resource_group.name, nsg).value!.body
+  create_security_rules(network_client, nsg, resource_group)
+  nsg
+end
+
+def create_security_rules(network_client, security_group, resource_group)
+  props = SecurityRulePropertiesFormat.new
+  props.priority = 100
+  props.source_port_range = '1-65000'
+  props.destination_port_range = '1-65000'
+  props.source_address_prefix = '*'
+  props.destination_address_prefix = '*'
+  props.protocol = 'Tcp' # 'Udp' or '*'
+  props.access = 'Deny'
+  props.direction = 'Inbound' # 'Outbound'
+  sr = SecurityRule.new
+  sr.properties = props
+  sr.name = "sr-#{TEST_NAME}"
+  network_client.security_rules.create_or_update(resource_group.name, security_group.name, sr.name, sr).value!.body
+end
+
+def create_ip_configuration(network_client, resource_group, subnet)
+  puts "     - creating ip"
+  ip_configuration = NetworkInterfaceIpConfiguration.new
+  ip_configuration_properties = NetworkInterfaceIpConfigurationPropertiesFormat.new
+  ip_configuration.properties = ip_configuration_properties
+  ip_configuration.name = 'ip_name-' + TEST_NAME
+  ip_configuration_properties.private_ipallocation_method = 'Dynamic'
+  ip_configuration_properties.public_ipaddress = create_public_ip_address(network_client, resource_group)
+  ip_configuration_properties.subnet = subnet
+  ip_configuration
+end
+
 def build_public_ip_params(location)
-  puts "     --- #{location}"
   public_ip = PublicIpAddress.new
   public_ip.location = location
   props = PublicIpAddressPropertiesFormat.new
   props.public_ipallocation_method = 'Dynamic'
   domain_name = 'domain-' + TEST_NAME
-  dns_settings = PublicIpAddressDnsSettings.new
-  dns_settings.domain_name_label = domain_name
-  props.dns_settings = dns_settings
+  # dns_settings = PublicIpAddressDnsSettings.new
+  # dns_settings.domain_name_label = domain_name
+  # props.dns_settings = dns_settings
   public_ip.properties = props
   public_ip
 end
@@ -131,7 +206,7 @@ def create_virtual_network(network_client, resource_group)
   network_client.virtual_networks.create_or_update(resource_group.name, virtualNetworkName, params).value!.body
 end
 
-def create_virtual_machine(compute_client, network_client, resource_group)
+def create_virtual_machine(compute_client, network_client, storage_client, resource_group)
   puts " * building OS profile"
   os_profile = OSProfile.new
   os_profile.computer_name = TEST_NAME
@@ -145,13 +220,26 @@ def create_virtual_machine(compute_client, network_client, resource_group)
   props.hardware_profile = hardware_profile
   puts " * building network profile"
   props.network_profile = create_network_profile(network_client, resource_group)
-  # puts " * building storage profile"
-  # props.storage_profile = create_storage_profile
+  puts " * building storage profile"
+  props.storage_profile = create_storage_profile(storage_client, resource_group)
   params = VirtualMachine.new
   params.type = 'Microsoft.Compute/virtualMachines'
   params.properties = props
   params.location = resource_group.location
+  puts " * deploying VM"
   promise = compute_client.virtual_machines.create_or_update(TEST_NAME, TEST_NAME, params)
+  result = promise.value!
+  result.body
+end
+
+def resource_group_exists?(resources_client, rg_name)
+  promise = resources_client.resource_groups.check_existence(rg_name)
+  result = promise.value!
+  resource_group = result.body
+end
+
+def resource_group_delete(resources_client, rg_name)
+  promise = resources_client.resource_groups.delete(rg_name)
   result = promise.value!
   result.body
 end
@@ -178,14 +266,24 @@ begin
   promise = compute_client.virtual_machines.list_all
   result = promise.value!
   list = result.body.value
-  puts " * Response (#{list.count} vms) : #{list.map(&:name)}"
+  puts " * Response (#{list.count} vms) : #{list}"
 
-  puts "Deleting all (#{list.count}) vms in the subscription..." unless list.empty?
-  # Delete the resource group, and all other resources will be deleted on cascade
-  list.each do |vm|
-    promise = resources_client.resource_groups.delete(vm.name)
-    result = promise.value!
-    puts " * Deleted #{vm.name} : #{result.body}"
+  # puts "Deleting all (#{list.count}) vms in the subscription..." unless list.empty?
+  # # Delete the resource group, and all other resources will be deleted on cascade
+  # list.each do |vm|
+  #   promise = resources_client.resource_groups.delete(vm.name)
+  #   result = promise.value!
+  #   puts " * Deleted #{vm.name} : #{result.body}"
+  # end
+
+  puts "Checking existence of resource group named '#{TEST_NAME}' ..."
+  rgpresent = resource_group_exists?(resources_client, TEST_NAME)
+  puts " * Resource group '#{TEST_NAME}'#{rgpresent ? '' : ' not'} present"
+
+  if rgpresent
+    puts "Deleting resource group named '#{TEST_NAME}' ..."
+    resource_group_delete(resources_client, TEST_NAME)
+    puts " * Resource group #{TEST_NAME} deleted"
   end
 
   puts "Creating a resource group named '#{TEST_NAME}' ..."
@@ -196,9 +294,9 @@ begin
   resource_group = result.body
   puts " * Created #{resource_group.name} with id '#{resource_group.id}' (#{resource_group.location})"
 
-  puts "Creating a virtual machine named '#{TEST_NAME}' ..."
-  vm = create_virtual_machine(compute_client, network_client, resource_group)
-  puts " * Created #{vm.name} with id : '#{vm.id}'"
+  # puts "Creating a virtual machine named '#{TEST_NAME}' ..."
+  # vm = create_virtual_machine(compute_client, network_client, storage_client, resource_group)
+  # puts " * Created #{vm.name} with id : '#{vm.id}'"
 
 
 
@@ -208,4 +306,5 @@ rescue MsRestAzure::AzureOperationError => ex
   puts " * request: #{ex.request}"
   puts " * response: #{ex.response}"
   puts " * body: #{ex.body}"
+  puts ex
 end
